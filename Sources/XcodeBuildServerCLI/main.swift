7ffd1cba85bd8ae0fc1e9e3a9ea3c0d1e1e36d47
Wang Lun <wang.lun@lycorp.co.jp>
2025-08-06 01:24:05 +0900

    update main to modern grammar

--- Sources/XcodeBuildServerCLI/main.swift
+++ Sources/XcodeBuildServerCLI/main.swift
@@ -7,33 +7,28 @@
 import Foundation
 import XcodeBuildServer
 
-private func run() {
-    let server = JSONRPCServer(
-        transport: StdioJSONRPCServerTransport(),
-        messageRegistry: bspRegistry,
-        messageHandler: XcodeBSPMessageHandler()
-    )
+@main
+struct XcodeBuildServerCLI {
+    static func main() async {
+        let server = JSONRPCServer(
+            transport: StdioJSONRPCServerTransport(),
+            messageRegistry: bspRegistry,
+            messageHandler: XcodeBSPMessageHandler()
+        )
 
-    Task { @MainActor in
+        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+            killSelfIfParentIsNull()
+        }
+
         await server.listen()
     }
 
-    Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
-        killSelfIfParentIsNull()
-    }
-
-    // Run the main loop to keep the program running and listening for changes
-    RunLoop.main.run()
-}
-
-private func killSelfIfParentIsNull() {
-    let parentProcessID = getppid()
-
-    // If parent process is 1, it's typically the init process, indicating the parent is null
-    if parentProcessID == 1 {
-        print("Parent process is null, killing self...")
-        exit(0) // Exit the process cleanly
+    private static func killSelfIfParentIsNull() {
+        let parentProcessID = getppid()
+        // If parent process is 1, it's typically the init process, indicating the parent is null
+        if parentProcessID == 1 {
+            print("Parent process is null, killing self...")
+            exit(0) // Exit the process cleanly
+        }
     }
 }
-
-run()
