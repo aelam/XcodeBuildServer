94a0a800f61d675d92af3cf693c378a3dddd1ff9
Wang Lun <wang.lun@lycorp.co.jp>
2025-08-07 14:48:18 +0900

    fixed request

--- Sources/XcodeBuildServer/BSPServer/Messages/build/BuildInitializeRequest.swift
+++ Sources/XcodeBuildServer/BSPServer/Messages/build/BuildInitializeRequest.swift
@@ -1,17 +1,5 @@
 //
-//  BuildInitializeRequest.swift
-//         logger.debug("BuildInitializeRequest.handle(ContextualMessageHandler) started for request ID: \(id)")
-        logger.debug("BuildInitializeRequest params - rootUri: \(params.rootUri)")
-
-        return await handler.withContext { context -> ResponseType in
-            do {
-                logger.debug("BuildInitializeRequest: starting loadProject with rootUri: \(self.params.rootUri)")
-                // Initialize the build server context with the project
-                try await context.loadProject(rootURL: URL(filePath: self.params.rootUri))
-                logger.debug("BuildInitializeRequest: loadProject completed successfully")
-
-                logger.debug("BuildInitializeRequest: checking URLs and index paths")Server
-//
+//  BuildInitializeRequestServer.swift
 //  Created by ST22956 on 2024/11/17.
 //
 
